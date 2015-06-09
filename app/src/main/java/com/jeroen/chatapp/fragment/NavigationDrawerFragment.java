327705b6820646af478c95c5edb0c110ae86343d
Jeroen <jeroen_nuyten@live.nl>
2015-06-09 19:05:31 +0200

    Drawer Menu with icons

--- app/src/main/java/com/jeroen/chatapp/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/jeroen/chatapp/fragment/NavigationDrawerFragment.java
@@ -1,5 +1,7 @@
 package com.jeroen.chatapp.fragment;
 
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.support.v7.app.ActionBar;
@@ -20,6 +22,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.jeroen.applicatie.R;
@@ -100,15 +103,22 @@ public class NavigationDrawerFragment extends Fragment {
                 selectItem(position);
             }
         });
+        final TypedArray typedArray = getResources().obtainTypedArray(R.array.sections_icons);
         mDrawerListView.setAdapter(new ArrayAdapter<String>(
                 getActionBar().getThemedContext(),
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
-                new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
-                }));
+                getResources().getStringArray(R.array.sections)
+        ) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View v = super.getView(position, convertView, parent);
+                int resourceId = typedArray.getResourceId(position, 0);
+                Drawable drawable = getResources().getDrawable(resourceId);
+                ((TextView) v).setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+                return v;
+            }
+        });
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
     }
@@ -250,9 +260,9 @@ public class NavigationDrawerFragment extends Fragment {
         }
 
         //if (item.getItemId() == R.id.action_example) {
-       //     Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-       //     return true;
-       // }
+        //     Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+        //     return true;
+        // }
 
         return super.onOptionsItemSelected(item);
     }
